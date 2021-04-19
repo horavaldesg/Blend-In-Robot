66c42e46dcc81a0e36958acb74badaecec64b805
Horacio Valdes <horavaldesg@gmail.com>
2021-04-19 15:44:55 -0400

    Spotlight reduction

--- Assets/Scripts/SpotLightDetection.cs
+++ Assets/Scripts/SpotLightDetection.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpotLightDetection : MonoBehaviour
 {
+    public Transform lightTransform;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +14,32 @@ public class SpotLightDetection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(lightTransform.position, lightTransform.forward, out hit))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                GetComponentInParent<Animator>().enabled = false;
+                GetComponentInParent<Light>().spotAngle = Mathf.Lerp(GetComponentInParent<Light>().spotAngle, 20, Time.deltaTime);
+            }
+
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         AnimatorClipInfo[] currentClip;
         currentClip = other.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0);
         Debug.Log(currentClip[0].clip.name);
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponentInParent<Light>().spotAngle = Mathf.Lerp(GetComponentInParent<Light>().spotAngle, 30, Time.deltaTime);
+        GetComponentInParent<Animator>().enabled = true;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+        
     }
 }
