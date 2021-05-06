5bbc43312a107b55ae900187dda80059cc12880b
Horacio Valdes <horavaldesg@gmail.com>
2021-05-06 17:18:31 -0400

    StopLight

---
+++ Assets/Scripts/StopLight.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopLight : MonoBehaviour
+{
+    GameObject[] lights;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = GameObject.FindGameObjectsWithTag("StopLight");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lights != null)
+        {
+           
+            lights = GameObject.FindGameObjectsWithTag("StopLight");
+            foreach (GameObject light in lights)
+            {
+                Color lightColor = light.GetComponent<Light>().color;
+                if (lightColor == Color.red)
+                {
+                    BehaviourScript.state = 2;
+                }
+                else if (lightColor == Color.red)
+                {
+                    BehaviourScript.state = 1;
+                }
+                else if(lightColor == Color.yellow)
+                {
+
+                }
+
+                if (BehaviourScript.state == 2)
+                {
+                    GameObject[] spawners = GameObject.FindGameObjectsWithTag("Spawner");
+                    foreach (GameObject spawner in spawners)
+                    {
+                        spawner.GetComponent<RobotSpawner>().enabled = false;
+                    }
+                }
+                else
+                {
+                    GameObject[] spawners = GameObject.FindGameObjectsWithTag("Spawner");
+                    foreach (GameObject spawner in spawners)
+                    {
+                        spawner.GetComponent<RobotSpawner>().enabled = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
