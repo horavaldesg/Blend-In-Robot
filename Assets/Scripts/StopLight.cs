1fdefda46393aa9a5dde7c4f86875e930829a835
Horacio Valdes <horavaldesg@gmail.com>
2021-05-06 18:03:51 -0400

    StopLight Working

--- Assets/Scripts/StopLight.cs
+++ Assets/Scripts/StopLight.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class StopLight : MonoBehaviour
 {
     GameObject[] lights;
+    [SerializeField] GameObject[] stopLights;
+    int lightLength = 0;
+    [SerializeField]int rate;
+
+    float i = 0;
     // Start is called before the first frame update
     void Start()
     {
         lights = GameObject.FindGameObjectsWithTag("StopLight");
+        foreach(GameObject light in stopLights)
+        {
+            light.SetActive(false);
+        }
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        i += Time.deltaTime;
+        //Debug.Log(i);
+        if(i > rate && i < rate + rate)
+        {
+            stopLights[0].SetActive(true);
+            stopLights[1].SetActive(false);
+            
+        }
+        else if(i > rate + rate && i < rate + rate + rate)
+        {
+            stopLights[0].SetActive(false);
+            stopLights[1].SetActive(true);
+            i = 0;
+        }
+        
         if (lights != null)
         {
            
@@ -25,7 +51,7 @@ public class StopLight : MonoBehaviour
                 {
                     BehaviourScript.state = 2;
                 }
-                else if (lightColor == Color.red)
+                else if (lightColor == Color.green)
                 {
                     BehaviourScript.state = 1;
                 }
